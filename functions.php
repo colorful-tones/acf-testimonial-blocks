b5cf671faa5d0dfea6d718fb87d48ae31309ada6
Damon Cook <damonacook@gmail.com>
2023-05-24 10:14:11 -0400

    👌 IMPROVE: PHP Doc blocks for registering ACF Blocks

--- functions.php
+++ functions.php
@@ -1,9 +1,22 @@
 <?php
-
-add_action( 'init', 'register_acf_blocks' );
+/**
+ * We use WordPress's init hook to make sure
+ * our blocks are registered early in the loading
+ * process.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/init/
+ */
 function register_acf_blocks() {
+	/**
+	 * We register our block's with WordPress's handy
+	 * register_block_type();
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/register_block_type/
+	 */
 	register_block_type( __DIR__ . '/blocks/testimonial-example-1' );
 	register_block_type( __DIR__ . '/blocks/testimonial-example-2' );
 	register_block_type( __DIR__ . '/blocks/testimonial-example-3' );
 	register_block_type( __DIR__ . '/blocks/testimonial-example-4' );
 }
+
+add_action( 'init', 'register_acf_blocks' );
