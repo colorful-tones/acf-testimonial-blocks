75fe38094caa464b8bd46f1f4bdca1db9554fe2f
Damon Cook <damonacook@gmail.com>
2023-05-25 12:39:07 -0400

    👌 IMPROVE: streamline example 1 code

--- blocks/testimonial-example-1/testimonial.php
+++ blocks/testimonial-example-1/testimonial.php
@@ -2,13 +2,7 @@
 /**
  * Testimonial Block Template (Example 1).
  *
- * @param array  $block The block settings and attributes.
- * @param string $content The block inner HTML (empty).
- * @param bool   $is_preview True during backend preview render.
- * @param int    $post_id The post ID the block is rendering content against.
- *                     This is either the post ID currently being displayed inside a query loop,
- *                     or the post ID of the post hosting this block.
- * @param array $context The context provided to the block by the post or it's parent block.
+ * @param array $block The block settings and attributes.
  */
 
 // Support custom "anchor" values.
